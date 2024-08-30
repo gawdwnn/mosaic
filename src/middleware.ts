ec30cbf99e249d74994e3132cc4a5523677df452
gd <gawdwnn@gmail.com>
2024-08-29 22:42:35 -0400

    setup, landing page

---
+++ src/middleware.ts
@@ -0,0 +1,10 @@
+import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export default authMiddleware({
+  publicRoutes: ["/site", "/api/uploadthing"],
+});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
